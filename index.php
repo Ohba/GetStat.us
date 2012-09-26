7eea13c017cb0755a2a3b5c2091d50382275e3b8
Andre Kradolfer <narfdre@gmail.com>
2012-09-25 21:36:14 -0600

    finsihing adding limonade

--- index.php
+++ index.php
@@ -1,14 +1,33 @@
 <?php 
 	require_once 'lib/limonade.php';
+	function configure()
+	{
+		$env = contains('localhost', $_SERVER['HTTP_HOST']) ? ENV_DEVELOPMENT : ENV_PRODUCTION;
+    	option('env', $env);
+		option('debug', true);
+		if(option('env') === ENV_DEVELOPMENT){
+			option('base_uri', 'getstatus');
+		}	
+	}
 
 	dispatch('/', 'hello');
 	  function hello(){
 	      return render('index.html.php');
 	  }
 
-	dispatch_get('/test', 'testing');
+	dispatch("/test", 'testing');
 	  function testing(){
-	  	  return "test";
-	  } 
+	  	  return 'test';
+	  }
 	run();
+
+	function contains($substring, $string) {
+        $pos = strpos($string, $substring);
+        if($pos === false) {
+            return false;
+        }
+        else {
+            return true;
+        }
+	}
 ?>
