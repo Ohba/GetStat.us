909df625172d494859d931bdc9d5465af63b4a76
Lauren Johnson <lauren_johnson@ymail.com>
2012-10-08 19:09:57 -0600

    db connection changes

--- db/connection.php
+++ db/connection.php
@@ -4,5 +4,10 @@
 	$user = "";
 	$pass = "";
 	$dbname = "";
+	$host = "instance27081.db.xeround.com";
+	$port = "17739";
+	$user = "getstatus";
+	$pass = "rohit";
+	$dbname = "dev";
 	$GLOBALS['database'] = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $pass);
 ?>
