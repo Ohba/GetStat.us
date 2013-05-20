a3f24a5aca9ab9bc31326c24b5ceba4cb80f5c0b
Dan Langford <danlangford@gmail.com>
2013-05-20 10:39:26 -0600

    some development code to quickly debug the currently logged in user

--- src/main/java/co/ohba/getstatus/resources/CloudResource.java
+++ src/main/java/co/ohba/getstatus/resources/CloudResource.java
@@ -3,6 +3,9 @@ package co.ohba.getstatus.resources;
 import co.ohba.getstatus.clouds.Cloud;
 import co.ohba.getstatus.services.CloudService;
 import com.google.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -13,6 +16,7 @@ import javax.ws.rs.core.Response;
 import java.net.MalformedURLException;
 import java.util.List;
 
+@Slf4j
 @Path("/clouds")
 @Produces(MediaType.APPLICATION_JSON)
 public class CloudResource {
@@ -20,8 +24,16 @@ public class CloudResource {
     @Inject
     CloudService service;
 
+    private void subjectLog() {
+        Subject s  = SecurityUtils.getSubject();
+        log.info("subject: {}", s);
+        log.info("hasRole? admin:{} guest:{} geek:{}", s.hasRole("admin"),s.hasRole("guest"),s.hasRole("geek"));
+        log.info("session: {}", s.getSession());
+    }
+
     @GET
     public Response getClouds() throws Exception {
+        subjectLog();
         List<Cloud> clouds = service.getClouds();
         return Response.ok().entity(clouds).build();
     }
@@ -29,6 +41,7 @@ public class CloudResource {
     @GET
     @Path("/{cloudName}")
     public Cloud getCloud(@PathParam("cloudName") String cloudName) throws MalformedURLException {
+        subjectLog();
         return service.getCloudStatus(cloudName);
     }
 
